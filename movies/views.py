ae882309814faa10e1d072dd3e8222931b01e4c0
Viktor Markovec <markvik42@gmail.com>
2021-11-14 17:39:08 +0300

    feat: add last_movies

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import redirect
 
 # Create your views here.
 from django.views import View
@@ -9,13 +9,11 @@ from .models import Movie
 
 
 class MoviesView(ListView):
-
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
 class MovieDetailView(DetailView):
-
     model = Movie
     slug_field = 'url'
 
