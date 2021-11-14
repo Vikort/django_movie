e862d7432af6eb45218f0b81f701e070bb54b2c3
Viktor Markovec <markvik42@gmail.com>
2021-11-14 15:42:30 +0300

    feat: split hmtl to templates, add movies shorts, add reviews

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.views import View
 from django.views.generic import ListView, DetailView
 
+from .forms import ReviewForm
 from .models import Movie
 
 
@@ -18,3 +19,15 @@ class MovieDetailView(DetailView):
 
     model = Movie
     slug_field = 'url'
+
+
+class AddReview(View):
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_path())
